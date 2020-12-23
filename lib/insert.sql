3779d38e382ff9fe1208ab673fc08fcc08747a08
Sierra Wallace <sskwallace@gmail.com>
2020-12-23 02:26:51 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,10 +1,11 @@
 	INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("First Project", "Easy", 500, "1/1", "2/3");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Easy Project", "Easy", 10000, "1/1", "2/3");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Hard Project", "Hard", 1000, "6/1", "6/3");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("The Project", "Hard", 100, "6/1", "6/3");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("A Project", "Fun", 600, "6/1", "6/3");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("The Simple Project", "Tolerable", 200, "6/1", "6/3");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("The Difficult Project", "Hard", 14400, "6/1", "6/3");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("The Fun Project", "Fun", 1023400, "6/1", "6/3");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Worst Project", "Hard", 100, "6/1", "6/3");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Best Project", "Fun", 20000, "6/1", "6/3");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Hard Project", "Hard", 1000, "1/1", "2/3");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("The Project", "Hard", 100, "1/1", "2/3");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("A Project", "Fun", 600, "1/1", "2/3");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("The Simple Project", "Tolerable", 200, "1/1", "2/3");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("The Difficult Project", "Hard", 14400, "1/1", "2/3");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("The Fun Project", "Fun", 1023400, "1/1", "2/3");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Worst Project", "Hard", 100, "1/1", "2/3");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Best Project", "Fun", 20000, "1/1", "2/3");
+
